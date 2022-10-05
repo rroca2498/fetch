8232709ddaf2205005433239a6888d38dcf87403
rroca2498 <rroca2498@outlook.com>
2022-10-04 21:12:28 -0300

    se agrego el archivo JSON

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,15 @@
-const articulos  = [
-    {sku: 1, descripcion: "bujia K4M Megane", precio:910 , rubro: "encendido" , img: "../Imagenes/repuestoMuestra.jpg"},
-    {sku: 2, descripcion: "manija interior Logan", precio: 3800, rubro: "accesorios" , img: "../Imagenes/repuestoMuestra.jpg"},
-    {sku: 3, descripcion: "kit cremallera embrague R21", precio: 1010, rubro:"accesorios" , img: "../Imagenes/repuestoMuestra.jpg"},
-    {sku: 4, descripcion: "pata caja R19", precio: 4840, rubro: "motor" , img: "../Imagenes/repuestoMuestra.jpg"},
-    {sku: 5, descripcion: "ruleman delantero SNR", precio: 7500, rubro: "rodamiento", img: "../Imagenes/repuestoMuestra.jpg"},
-    {sku: 6, descripcion: "ruleman trasero SKF", precio: 6950, rubro: "rodamiento", img: "../Imagenes/repuestoMuestra.jpg"},
-    {sku: 7, descripcion: "bieleta Suran", precio: 2870, rubro: "suspension", img: "../Imagenes/repuestoMuestra.jpg"},
-    {sku: 8, descripcion: "amortiguador delantero Duster", precio: 15640, rubro: "suspension", img: "../Imagenes/repuestoMuestra.jpg"},
-    {sku: 9, descripcion: "antena universal", precio: 1500, rubro: "accesorios", img: "../Imagenes/repuestoMuestra.jpg"},
-    {sku: 10, descripcion: "cables de bujia K7M", precio: 11200, rubro: "encendido", img: "../Imagenes/repuestoMuestra.jpg"}
-];
+// const articulos  = [
+//     {sku: 1, descripcion: "bujia K4M Megane", precio:910 , rubro: "encendido" , img: "../Imagenes/repuestoMuestra.jpg"},
+//     {sku: 2, descripcion: "manija interior Logan", precio: 3800, rubro: "accesorios" , img: "../Imagenes/repuestoMuestra.jpg"},
+//     {sku: 3, descripcion: "kit cremallera embrague R21", precio: 1010, rubro:"accesorios" , img: "../Imagenes/repuestoMuestra.jpg"},
+//     {sku: 4, descripcion: "pata caja R19", precio: 4840, rubro: "motor" , img: "../Imagenes/repuestoMuestra.jpg"},
+//     {sku: 5, descripcion: "ruleman delantero SNR", precio: 7500, rubro: "rodamiento", img: "../Imagenes/repuestoMuestra.jpg"},
+//     {sku: 6, descripcion: "ruleman trasero SKF", precio: 6950, rubro: "rodamiento", img: "../Imagenes/repuestoMuestra.jpg"},
+//     {sku: 7, descripcion: "bieleta Suran", precio: 2870, rubro: "suspension", img: "../Imagenes/repuestoMuestra.jpg"},
+//     {sku: 8, descripcion: "amortiguador delantero Duster", precio: 15640, rubro: "suspension", img: "../Imagenes/repuestoMuestra.jpg"},
+//     {sku: 9, descripcion: "antena universal", precio: 1500, rubro: "accesorios", img: "../Imagenes/repuestoMuestra.jpg"},
+//     {sku: 10, descripcion: "cables de bujia K7M", precio: 11200, rubro: "encendido", img: "../Imagenes/repuestoMuestra.jpg"}
+// ];
 
 const encontrado = document.querySelector("#listaProductos");
 
@@ -21,21 +21,14 @@ const desplegarCarrito = document.querySelector(".productosEnElCarrito");
 
 const carrito = [];
 
-  articulos.forEach((producto) => {
+
+ 
+
+function imprimirProductos(arr){
+  arr.forEach((producto) => {
     const div = document.createElement('div');
     div.classList.add('producto');
 
-    // Quera abreviarlo asi pero no me funcionabam en que me estoy equivocando?
-
-    // let {sku,descripcion,precio,rubro,img} = item;
-
-    // div.innerHTML = `
-    // <img src="../Imagenes/repuestoMuestra.jpg" alt="Imagen de ${descripcion}">
-    // <h3>${descripcion}</h3>
-    // <p>$${precio}</p>
-    // <button id="agregar${sku}">Agregar</button>
-    // `
-    
     div.innerHTML = `
     <img src="../Imagenes/repuestoMuestra.jpg" alt="Imagen de ${producto.descripcion}">
     <h3>${producto.descripcion}</h3>
@@ -67,7 +60,7 @@ const carrito = [];
 
     
 
-    const item = articulos.find((prod) => prod.sku === prodId);
+    const item = arr.find((prod) => prod.sku === prodId);
     carrito.push(item);
     console.log(carrito);
 
@@ -93,7 +86,15 @@ const carrito = [];
 
     
   }
- 
+
+}
+
+fetch('../JS/data.json')
+.then(res=>res.json())
+.then(data=>imprimirProductos(data));
+
+  
+  
  
 
  
